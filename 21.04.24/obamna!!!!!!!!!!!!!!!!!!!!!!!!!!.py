eaff9844e2802ed2ad09cc1eb93f990ee53d51b8
tviooshko <167680789+tviooshko@users.noreply.github.com>
2024-05-05 17:59:15 +0700

    obamna!!!!!!!!!!!!!!!!!!!!!!!!!!.py

--- 21.04.24/obamna!!!!!!!!!!!!!!!!!!!!!!!!!!.py
+++ 21.04.24/obamna!!!!!!!!!!!!!!!!!!!!!!!!!!.py
@@ -1,33 +1,52 @@
 import random
+def atack(q,enemyhealth,attack):
+    q=random.randint(1,2)
+    if q==1:
+        enemyhealth=enemyhealth-attack
+    return(enemyhealth)
+def eattack(enemyattack,q,dhealth):
+    q=random.randint(1,2)
+    if q==1:
+        dhealth=dhealth-enemyattack
+    return(dhealth)
+def heal(dhealth,enemyattack):
+    dhealth=dhealth+((wave+enemyattack)//2)
+    return(dhealth)
+
+print('Напишите 1 для атаки, 2 для защиты. Ваша задача победить столько врагов сколько сможете.')
 w=0
+q=0
+
 a1='слизень'
 a2='рыцарь'
 a3='дракон'
+
 attack=20
 health=100
 dhealth=health
-mastery=2
-lvl=0
+
 wave=0
 index=0
+
 enemyhealth=0
 enemyattack=0
+
+print('')
 while dhealth>0:
     if wave==12:
         mastery=1
     index=random.randint(1,7)
     if index==1 or index==2 or index==3 or index==4 or wave<3:
-        
         enemyhealth=40+wave*2
         enemyattack=10+wave*2
         print("на вас напал "+a1+' '+str(enemyhealth)+' здоровья'+' '+str(enemyattack)+' атк')
         while enemyhealth>0 and dhealth>0:
             w=input()
-            if w=='attack':
-                w=random.randint(1,mastery)
-                if w==1:
-                    enemyhealth=enemyhealth-attack
-            dhealth-=enemyattack
+            if w=='1':
+                enemyhealth=atack(q,enemyhealth,attack)
+            dhealth=eattack(enemyattack,q,dhealth)
+            if w=='2':
+                dhealth=heal(dhealth,enemyattack)
             print(dhealth,enemyhealth)
         health+=5
         attack+=5
@@ -39,11 +58,11 @@ while dhealth>0:
         print("на вас напал "+a2+' '+str(enemyhealth)+' здоровья'+' '+str(enemyattack)+' атк')
         while enemyhealth>0 and dhealth>0:
             w=input()
-            if w=='attack':
-                w=random.randint(1,mastery)
-                if w==1:
-                    enemyhealth=enemyhealth-attack
-            dhealth-=enemyattack
+            if w=='1':
+                enemyhealth=atack(q,enemyhealth,attack)
+            dhealth=eattack(enemyattack,q,dhealth)
+            if w=='2':
+                dhealth=heal(dhealth,enemyattack)
             print(dhealth,enemyhealth)
         health+=10
         attack+=10
@@ -55,18 +74,18 @@ while dhealth>0:
         print("на вас напал "+a3+' '+str(enemyhealth)+' здоровья'+' '+str(enemyattack)+' атк')
         while enemyhealth>0 and dhealth>0:
             w=input()
-            if w=='attack':
-                w=random.randint(1,mastery)
-                if w==1:
-                    enemyhealth=enemyhealth-attack
-            dhealth-=enemyattack
+            if w=='1':
+                enemyhealth=attack(q,enemyhealth,attack)
+            dhealth=eattack(enemyattack,q,dhealth)
+            if w=='2':
+                dhealth=heal(dhealth,enemyattack)
             print(dhealth,enemyhealth)
         wave+=1
-        
         health+=20
         attack+=20
+
     if dhealth<=0:
         break
     dhealth=health
-wave=str(wave)
-print('вы умерли,вы пережили '+wave+' волн')
+wave=str(wave-1)
+print('вы умерли,вы победили '+wave+' врагов')
